f59857c2ad7c55df690cdc8ee2f76a597c19a07b
nicbritos <nicbritos@hotmail.com>
2020-04-10 20:39:55 -0300

    Sprint 1 - Added DDL and data

---
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/sql/ddl.sql
@@ -0,0 +1,107 @@
+create table system_country
+(
+	country_id varchar(2) default NULL::character varying not null
+		constraint system_country_pk
+			primary key,
+	name text not null
+);
+
+alter table system_country owner to postgres;
+
+create unique index system_country_country_id_uindex
+	on system_country (country_id);
+
+create table system_province
+(
+	province_id serial not null
+		constraint system_province_pk
+			primary key,
+	country_id varchar(2) not null
+		constraint system_province_country
+			references system_country
+				on update restrict on delete cascade,
+	name text not null
+);
+
+alter table system_province owner to postgres;
+
+create table office
+(
+	office_id serial not null
+		constraint office_pk
+			primary key,
+	name text not null,
+	street text,
+	province_id integer not null
+		constraint office_province_id
+			references system_province
+				on update restrict on delete restrict,
+	phone text,
+	email text,
+	street_number integer not null
+);
+
+alter table office owner to postgres;
+
+create unique index office_office_id_uindex
+	on office (office_id);
+
+create unique index system_province_province_id_uindex
+	on system_province (province_id);
+
+create table staff
+(
+	staff_id serial not null
+		constraint staff_pk
+			primary key,
+	office_id integer not null
+		constraint staff_office
+			references office
+				on update restrict on delete cascade,
+	first_name text not null,
+	surname text not null,
+	phone text,
+	email text,
+	registration_number integer not null
+);
+
+alter table staff owner to postgres;
+
+create unique index staff_staff_id_uindex
+	on staff (staff_id);
+
+create table system_staff_specialty
+(
+	specialty_id serial not null
+		constraint specialty_pk
+			primary key,
+	name text not null
+);
+
+alter table system_staff_specialty owner to postgres;
+
+create unique index specialty_specialty_id_uindex
+	on system_staff_specialty (specialty_id);
+
+create table system_staff_specialty_staff
+(
+	specialty_id integer not null
+		constraint specialty_staff_system_specialty
+			references system_staff_specialty
+				on update restrict on delete restrict,
+	staff_id integer not null
+		constraint specialty_staff_staff
+			references staff
+				on update restrict on delete cascade,
+	constraint specialty_staff_pk
+		primary key (staff_id, specialty_id)
+);
+
+alter table system_staff_specialty_staff owner to postgres;
+
+create unique index specialty_staff_specialty_id_uindex
+	on system_staff_specialty_staff (specialty_id);
+
+create unique index specialty_staff_staff_id_uindex
+	on system_staff_specialty_staff (staff_id);
+
