26a7568c0eb7cf9671ff1cfbe49e73984b329956
aroca <aroca@itba.edu.ar>
2020-04-13 00:23:29 -0300

    Setting up everything for DAO's tests

---
+++ persistence/src/test/java/ar/edu/itba/paw/CountryDaoImplTest.java
@@ -0,0 +1,65 @@
+package ar.edu.itba.paw;
+
+import ar.edu.itba.paw.models.Country;
+import ar.edu.itba.paw.persistence.CountryDaoImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import javax.sql.DataSource;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Sql(scripts = "classpath:sql/schema.sql")
+@ContextConfiguration(classes = TestConfig.class)
+public class CountryDaoImplTest
+{
+    private static final String NAME = "testName";
+    private static final String STREET = "Av 9 de Julio";
+    private static final String PROVINCE = "Buenos Aires";
+    private static final String PHONE = "1234567890";
+    private static final String EMAIL = "test@test.com";
+    private static final int STREET_NUMBER = 123;
+    private static final String COUNTRY = "Argentina";
+    private static final String COUNTRY_ID = "AR";
+
+    private static final String COUNTRIES_TABLE = "system_country";
+
+    private CountryDaoImpl countryDao;
+    private JdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert jdbcInsert;
+
+    @Autowired
+    private DataSource ds;
+
+    @Before
+    public void setUp(){
+        countryDao = new CountryDaoImpl(ds);
+        jdbcTemplate = new JdbcTemplate(ds);
+        jdbcInsert = new SimpleJdbcInsert(ds)
+                .withTableName(COUNTRIES_TABLE);
+
+    }
+
+    @Test
+    public void testCreateCountry()
+    {
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, COUNTRIES_TABLE);
+        Country c = new Country();
+        c.setName(COUNTRY);
+        c.setId(COUNTRY_ID);
+        c.setProvinces(null);
+        Country county = countryDao.create(c);
+
+        assertEquals(1, JdbcTestUtils.countRowsInTable(jdbcTemplate, COUNTRIES_TABLE));
+    }
+}
---
+++ persistence/src/test/resources/sql/schema.sql
@@ -0,0 +1,75 @@
+create table if not exists system_country
+(
+    country_id varchar(2) default NULL
+        constraint system_country_pk
+            primary key,
+    name varchar(255) not null
+);
+
+create table if not exists system_province
+(
+    province_id identity not null
+        constraint system_province_pk
+            primary key,
+    country_id varchar(2) not null
+        constraint system_province_country
+            references system_country
+--             on update restrict on delete cascade
+,
+    name varchar(255) not null
+);
+
+create table if not exists office
+(
+    office_id identity not null
+        constraint office_pk
+            primary key,
+    name varchar(255) not null,
+    street varchar(255),
+    province_id integer not null
+        constraint office_province_id
+            references system_province
+--             on update restrict on delete restrict
+,
+    phone varchar(255),
+    email varchar(255),
+    street_number integer not null
+);
+
+create table if not exists staff
+(
+    staff_id identity not null
+        constraint staff_pk
+            primary key,
+    office_id integer not null
+        constraint staff_office
+            references office
+--             on update restrict on delete cascade
+,
+    first_name varchar(255) not null,
+    surname varchar(255) not null,
+    phone varchar(255),
+    email varchar(255),
+    registration_number integer not null
+);
+
+create table if not exists system_staff_specialty
+(
+    specialty_id identity not null
+        constraint specialty_pk
+            primary key,
+    name varchar(255) not null
+);
+
+create table if not exists system_staff_specialty_staff
+(
+    specialty_id integer not null
+        constraint specialty_staff_system_specialty
+            references system_staff_specialty
+--             on update restrict on delete restrict
+,
+    staff_id integer not null
+        constraint specialty_staff_staff
+            references staff
+--             on update restrict on delete cascade
+);
