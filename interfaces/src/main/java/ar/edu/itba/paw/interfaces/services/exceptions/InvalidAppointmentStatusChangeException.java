8824f8f01f3d134b02b61302b65519b65e547cc9
aroca <aroca@itba.edu.ar>
2020-05-01 15:13:44 -0300

    Service test WIP

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/services/exceptions/InvalidAppointmentStatusChangeException.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/services/exceptions/InvalidAppointmentStatusChangeException.java
@@ -3,7 +3,7 @@ package ar.edu.itba.paw.interfaces.services.exceptions;
 import ar.edu.itba.paw.interfaces.MediCareException;
 
 public class InvalidAppointmentStatusChangeException extends MediCareException {
-    public InvalidAppointmentStatusChangeException() {
-        super("Estado invalido");
+    public InvalidAppointmentStatusChangeException(String formStatus, String toStatus) {
+        super("No se puede cambiar este turno del estado " + formStatus + " al estado " + toStatus);
     }
 }
