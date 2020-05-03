aeb9d46ec30436d5295fd83a436d2dd52438f1aa
nicbritos <nicbritos@hotmail.com>
2020-05-03 19:48:17 -0300

    Sprint 2 - Fix AppointmentTimeSlotTransformer

--- webapp/src/main/java/ar/edu/itba/paw/webapp/transformer/AppointmentTimeSlotTransformer.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/transformer/AppointmentTimeSlotTransformer.java
@@ -2,7 +2,7 @@ package ar.edu.itba.paw.webapp.transformer;
 
 import ar.edu.itba.paw.models.Appointment;
 import ar.edu.itba.paw.models.AppointmentTimeSlot;
-import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
@@ -13,7 +13,7 @@ public class AppointmentTimeSlotTransformer extends GenericTransformer<Appointme
     public Map<String, ?> transform(AppointmentTimeSlot timeSlot) {
         Map<String, Object> map = new HashMap<>();
         map.put("hour", timeSlot.getDate().getHourOfDay());
-        map.put("minute", timeSlot.getDate().getMinuteOfDay());
+        map.put("minute", timeSlot.getDate().getMinuteOfHour());
         map.put("duration", Appointment.DURATION);
         return map;
     }
@@ -22,13 +22,13 @@ public class AppointmentTimeSlotTransformer extends GenericTransformer<Appointme
     public Collection<Map<String, ?>> transform(Collection<AppointmentTimeSlot> appointmentTimeSlots) {
         Collection<Map<String, ?>> transformed = new LinkedList<>();
 
-        Map<DateTime, List<Map<String, ?>>> transformedTimeSlotsPerDay = new HashMap<>();
+        Map<LocalDate, List<Map<String, ?>>> transformedTimeSlotsPerDay = new HashMap<>();
         for (AppointmentTimeSlot appointmentTimeSlot : appointmentTimeSlots) {
-            List<Map<String, ?>> transformedTimeSlots = transformedTimeSlotsPerDay.computeIfAbsent(appointmentTimeSlot.getDate(), k -> new LinkedList<>());
+            List<Map<String, ?>> transformedTimeSlots = transformedTimeSlotsPerDay.computeIfAbsent(appointmentTimeSlot.getDate().toLocalDate(), k -> new LinkedList<>());
             transformedTimeSlots.add(this.transform(appointmentTimeSlot));
         }
 
-        for (Map.Entry<DateTime, List<Map<String, ?>>> dateList : transformedTimeSlotsPerDay.entrySet()) {
+        for (Map.Entry<LocalDate, List<Map<String, ?>>> dateList : transformedTimeSlotsPerDay.entrySet()) {
             Map<String, Object> map = new HashMap<>();
             map.put("date", this.transformDate(dateList.getKey()));
             map.put("timeslots", dateList.getValue());
@@ -38,7 +38,7 @@ public class AppointmentTimeSlotTransformer extends GenericTransformer<Appointme
         return transformed;
     }
 
-    private Map<String, ?> transformDate(DateTime date) {
+    private Map<String, ?> transformDate(LocalDate date) {
         Map<String, Object> map = new HashMap<>();
         map.put("year", date.getYear());
         map.put("month", date.getMonthOfYear());
