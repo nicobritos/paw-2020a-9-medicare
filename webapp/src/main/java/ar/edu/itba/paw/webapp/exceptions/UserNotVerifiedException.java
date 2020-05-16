47e1ac7c204fa9a56e61b5bdb48a7c793b4cbcc6
nicbritos <nicbritos@hotmail.com>
2020-05-15 22:29:41 -0300

    Sprint 4 - Hacer que los roles se cambien solos en todas las pestañas luego de verificar una cuenta

--- webapp/src/main/java/ar/edu/itba/paw/webapp/exceptions/UserNotVerifiedException.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.itba.paw.webapp.exceptions;
-
-import ar.edu.itba.paw.interfaces.MediCareException;
-
-public class UserNotVerifiedException extends MediCareException {
-    public UserNotVerifiedException() {
-        super("La cuenta no se encuentra confirmada");
-    }
-}
