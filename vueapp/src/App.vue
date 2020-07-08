3afaa50a5442e9e0760de947a00045510aa33c98
Guido Barbieri <gbarbieri@itba.edu.ar>
2020-07-08 02:28:25 -0300

    changed extensions, more vueiing done

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <navbar></navbar>
     <router-view/>
   </div>
 </template>
+
+<script>
+import { navbar } from "./components/navbar/navbar.vue";
+
+
+export default {
+  name:"App",
+  components:[
+    navbar
+  ]
+}
+</script>
 
 <style>
 </style>
---
+++ vueapp/src/components/navbar/navbar.vue
@@ -0,0 +1,26 @@
+//
+<c:choose>
+//     <c:when test="${user.isPresent()}">
+//         <%@ include file="navbarLogged.jsp" %>
+//     </c:when>
+//     <c:otherwise>
+//         <%@ include file="navbarNotLogged.jsp" %>
+//     </c:otherwise>
+// </c:choose>
+<template>
+  <!--TODO:check -->
+  <div>
+    <NavbarLogged v-if="user.isPreset()"></NavbarLogged>
+    <NavbarNotLogged v-else></NavbarNotLogged>
+  </div>
+</template>
+
+<script>
+import { NavbarLogged } from "./navbarLogged";
+import { NavbarNotLogged } from "./navbarNotLogged";
+
+export default {
+  name: "Navbar",
+  components: [NavbarLogged, NavbarNotLogged],
+};
+</script>
