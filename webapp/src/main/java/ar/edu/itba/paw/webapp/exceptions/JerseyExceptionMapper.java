347f33a5c9bcd4713fe90386ab8df3eba00fda77
nicbritos <nicbritos@hotmail.com>
2020-07-10 03:49:17 -0300

    Fixed some generic errors sent via mappers

--- webapp/src/main/java/ar/edu/itba/paw/webapp/exceptions/JerseyExceptionMapper.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/exceptions/JerseyExceptionMapper.java
@@ -1,6 +1,11 @@
 package ar.edu.itba.paw.webapp.exceptions;
 
+import ar.edu.itba.paw.webapp.media_types.ErrorMIME;
+import ar.edu.itba.paw.webapp.models.APIError;
+
+import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -9,7 +14,18 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class JerseyExceptionMapper implements ExceptionMapper<WebApplicationException> {
     @Override
+    @Produces({ErrorMIME.ERROR, MediaType.TEXT_PLAIN})
     public Response toResponse(WebApplicationException exception) {
-        return Response.status(exception.getResponse() != null ? exception.getResponse().getStatus() : Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity("").build();
+        if (exception.getResponse() != null) {
+            return Response
+                    .status(exception.getResponse().getStatus())
+                    .entity(!(exception.getResponse().getEntity() instanceof APIError) ? APIErrorBuilder.buildError(exception.getResponse().getStatus()) : "")
+                    .build();
+        } else {
+            return Response
+                    .status(Status.INTERNAL_SERVER_ERROR)
+                    .entity(APIErrorBuilder.buildError(Status.INTERNAL_SERVER_ERROR))
+                    .build();
+        }
     }
 }
--- webapp/src/main/java/ar/edu/itba/paw/webapp/exceptions/NotFoundExceptionMapper.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/exceptions/NotFoundExceptionMapper.java
@@ -1,5 +1,7 @@
 package ar.edu.itba.paw.webapp.exceptions;
 
+import ar.edu.itba.paw.webapp.models.APIError;
+
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -10,6 +12,16 @@ import javax.ws.rs.ext.Provider;
 public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
     @Override
     public Response toResponse(NotFoundException exception) {
-        return Response.status(exception.getResponse() != null ? exception.getResponse().getStatus() : Status.NOT_FOUND.getStatusCode()).entity("").build();
+        if (exception.getResponse() != null) {
+            return Response
+                    .status(exception.getResponse().getStatus())
+                    .entity(!(exception.getResponse().getEntity() instanceof APIError) ? APIErrorBuilder.buildError(exception.getResponse().getStatus()) : "")
+                    .build();
+        } else {
+            return Response
+                    .status(Status.NOT_FOUND)
+                    .entity(APIErrorBuilder.buildError(Status.NOT_FOUND))
+                    .build();
+        }
     }
 }
