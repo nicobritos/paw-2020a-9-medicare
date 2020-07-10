1451ddfaeab62c753e9780c82d3010bee34ace68
nicbritos <nicbritos@hotmail.com>
2020-07-10 03:58:57 -0300

    Fixed content type in errors sent via mappers

--- webapp/src/main/java/ar/edu/itba/paw/webapp/exceptions/JerseyExceptionMapper.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/exceptions/JerseyExceptionMapper.java
@@ -3,7 +3,6 @@ package ar.edu.itba.paw.webapp.exceptions;
 import ar.edu.itba.paw.webapp.media_types.ErrorMIME;
 import ar.edu.itba.paw.webapp.models.APIError;
 
-import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -14,17 +13,28 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class JerseyExceptionMapper implements ExceptionMapper<WebApplicationException> {
     @Override
-    @Produces({ErrorMIME.ERROR, MediaType.TEXT_PLAIN})
     public Response toResponse(WebApplicationException exception) {
         if (exception.getResponse() != null) {
+            Object entity;
+            String mediaType;
+            if (!(exception.getResponse().getEntity() instanceof APIError)) {
+                entity = APIErrorBuilder.buildError(exception.getResponse().getStatus());
+                mediaType = ErrorMIME.ERROR;
+            } else {
+                entity = "";
+                mediaType = MediaType.TEXT_PLAIN;
+            }
+
             return Response
                     .status(exception.getResponse().getStatus())
-                    .entity(!(exception.getResponse().getEntity() instanceof APIError) ? APIErrorBuilder.buildError(exception.getResponse().getStatus()) : "")
+                    .entity(entity)
+                    .type(mediaType)
                     .build();
         } else {
             return Response
                     .status(Status.INTERNAL_SERVER_ERROR)
                     .entity(APIErrorBuilder.buildError(Status.INTERNAL_SERVER_ERROR))
+                    .type(ErrorMIME.ERROR)
                     .build();
         }
     }
--- webapp/src/main/java/ar/edu/itba/paw/webapp/exceptions/NotFoundExceptionMapper.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/exceptions/NotFoundExceptionMapper.java
@@ -1,8 +1,10 @@
 package ar.edu.itba.paw.webapp.exceptions;
 
+import ar.edu.itba.paw.webapp.media_types.ErrorMIME;
 import ar.edu.itba.paw.webapp.models.APIError;
 
 import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -13,14 +15,26 @@ public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundExceptio
     @Override
     public Response toResponse(NotFoundException exception) {
         if (exception.getResponse() != null) {
+            Object entity;
+            String mediaType;
+            if (!(exception.getResponse().getEntity() instanceof APIError)) {
+                entity = APIErrorBuilder.buildError(exception.getResponse().getStatus());
+                mediaType = ErrorMIME.ERROR;
+            } else {
+                entity = "";
+                mediaType = MediaType.TEXT_PLAIN;
+            }
+
             return Response
                     .status(exception.getResponse().getStatus())
-                    .entity(!(exception.getResponse().getEntity() instanceof APIError) ? APIErrorBuilder.buildError(exception.getResponse().getStatus()) : "")
+                    .entity(entity)
+                    .type(mediaType)
                     .build();
         } else {
             return Response
                     .status(Status.NOT_FOUND)
                     .entity(APIErrorBuilder.buildError(Status.NOT_FOUND))
+                    .type(ErrorMIME.ERROR)
                     .build();
         }
     }
