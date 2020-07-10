f8430f986988df717de0b304f5bceef7cd85b545
Guido Barbieri <gbarbieri@itba.edu.ar>
2020-07-10 01:53:37 -0300

    """finished""" login

--- vueapp/src/views/authentication/login.vue
+++ vueapp/src/views/authentication/login.vue
@@ -1,73 +1,151 @@
-<html>
-<head>
-    <%@ include file="../../partials/head.jsp" %>
-    <link rel="stylesheet" href='<c:url value="/css/authentication/login.css"/> '>
-</head>
-<body>
-<div class="container w-100 h-100 d-flex flex-column justify-content-center align-items-center">
-    <c:choose>
-        <c:when test="${token != null}">
-            <c:url value="/login?token=${token}" var="loginUrl"/>
-        </c:when>
-        <c:otherwise>
-            <c:url value="/login" var="loginUrl"/>
-        </c:otherwise>
-    </c:choose>
-    <c:url value="/signup" var="signupUrl"/>
-    <form:form modelAttribute="loginForm" class="register-form border p-5 rounded" action="${loginUrl}" method="POST"
-               enctype="application/x-www-form-urlencoded">
-        <div class="row">
-            <h6>Medicare <img src='<c:url value="/img/logo.svg"/>' id="logo"/></h6>
-        </div>
-        <div class="row justify-content-start">
-            <h1 class="register-form-title"><spring:message code="Login"/></h1>
-        </div>
-        <div class="form-group row">
-            <div class="col">
-                <label for="medicare_email"><spring:message code="Email"/></label>
+<template>
+    <div class="container w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+        <form class="register-form border p-5 rounded">
+            <div class="row">
+                <h6>Medicare <img :src='logo' id="logo"/></h6>
             </div>
-            <div class="col-8">
-                <form:input path="email" class="form-control" type="email" name="medicare_email" id="medicare_email"/>
+            <div class="row justify-content-start">
+                <h1 class="register-form-title">{{$t("Login")}}</h1>
             </div>
-        </div>
-        <div class="form-group row">
-            <div class="col">
-                <label for="medicare_password"><spring:message code="Password"/></label>
+            <div class="form-group row">
+                <div class="col">
+                    <label for="medicare_email">{{$t("Email")}}</label>
+                </div>
+                <div class="col-8">
+                    <input class="form-control" type="email" name="medicare_email" id="medicare_email"/>
+                </div>
             </div>
-            <div class="col-8">
-                <form:input path="password" class="form-control pr-5" type="password" name="medicare_password"
-                            id="medicare_password"/>
-                <!-- For this to work for must be the id of the password input -->
-                <label for="medicare_password" class="toggle-visibility"><img src='<c:url value="/img/eye.svg"/> '><img
-                        src='<c:url value="/img/noeye.svg"/>' style="display: none;"></label>
+            <div class="form-group row">
+                <div class="col">
+                    <label for="medicare_password">{{$t("Password")}}</label>
+                </div>
+                <div class="col-8">
+                    <input class="form-control pr-5" :type='showPassword?"text":"password"' name="medicare_password" id="medicare_password"/>
+                    <!-- For this to work for must be the id of the password input -->
+                    <label for="medicare_password" class="toggle-visibility" @click="toggleShowPassword()">
+                        <img v-if="!showPassword" :src='eye'>
+                        <img v-else :src='noeye'>
+                    </label>
+                </div>
             </div>
-        </div>
-        <div class="form-group row align-items-center">
-            <div class="col">
-                <label for="medicare_remember_me" class="mb-0"><spring:message code="RememberMe"/></label>
+            <div class="form-group row align-items-center">
+                <div class="col">
+                    <label for="medicare_remember_me" class="mb-0">{{$t("RememberMe")}}</label>
+                </div>
+                <div class="col-8">
+                    <checkbox id="medicare_remember_me" name="medicare_remember_me"/>
+                </div>
             </div>
-            <div class="col-8">
-                <form:checkbox path="rememberMe" id="medicare_remember_me" name="medicare_remember_me"/>
+            <div class="form-row justify-content-between align-items-end mt-2">
+                <a class="form-link" :href="signupUrl">{{$t("CreateAccount")}}</a>
+                <button type="submit" class="btn btn-primary">{{$t("Confirm")}}</button>
             </div>
-        </div>
-        <div class="form-row justify-content-between align-items-end mt-2">
-            <a class="form-link" href="${signupUrl}"><spring:message code="CreateAccount"/></a>
-            <button type="submit" class="btn btn-primary"><spring:message code="Confirm"/></button>
-        </div>
-
-        <form:errors path="*" cssClass="mt-4 mb-0 text-danger" element="p"/>
-        <c:if test="${not empty SPRING_SECURITY_LAST_EXCEPTION}">
-            <p class="mt-4 mb-0 text-danger">
-                <spring:message code="InvalidCredentials.loginForm"/>
+            <p v-if="invalidCredentials" class="mt-4 mb-0 text-danger">
+                {{$t("InvalidCredentials.loginForm")}}
             </p>
-        </c:if>
-    </form:form>
-</div>
-<script src='<c:url value="/js/scripts/authentication/Login.js"/> '></script>
+        </form>
+    </div>
+</template>
+
 <script>
-    $(document).ready(() => {
-        Login.init();
-    })
+import logo from "@/assets/logo.svg";
+//TODO:change to bootstrap icons
+import eye from "@/assets/eye.svg";
+import noeye from "@/assets/noeye.svg";
+
+export default {
+    name:"Login",
+    data:function() {
+        return{
+            logo:logo,
+            eye:eye,
+            noeye:noeye,
+            showPassword:false
+        }
+    },
+    methods:{
+        toggleShowPassword(){
+            this.showPassword = !this.showPassword;
+        }
+    }
+}
 </script>
-</body>
-</html>
+
+<style>
+body, html {
+    height: 100%;
+    background-color: rgba(0, 196, 186, 0.205);
+}
+
+.register-form {
+    background-color: #fff;
+    border-radius: 1em !important;
+    box-shadow: 10px 9px 12px 0px rgba(0, 196, 186, 0.205);
+    max-width: 430px;
+    width: 430px;
+}
+
+
+.form-link:hover {
+    text-decoration: none;
+}
+
+.register-form input {
+    background-color: #f0f0f0;
+}
+
+.register-form input:focus {
+    background-color: #e0e0e0;
+}
+
+.register-form-title {
+    margin-bottom: 1em;
+}
+
+.register-form #logo {
+    width: 1em;
+}
+
+.register-form button {
+    background-color: #00C4BA;
+    color: white;
+}
+
+.register-form button:hover {
+    background-color: rgb(1, 150, 142);
+    color: #fafafa;
+}
+
+.form-password {
+    position: relative;
+}
+
+.toggle-visibility {
+    position: absolute;
+    right: 2em;
+    top: 0.6em;
+    bottom: auto;
+    left: auto;
+    z-index: 1;
+
+    cursor: pointer;
+}
+
+.profile-picture-container {
+    display: inline-block;
+    position: relative;
+    width: 100%;
+}
+
+.profile-picture {
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    position: absolute;
+}
+
+</style>
