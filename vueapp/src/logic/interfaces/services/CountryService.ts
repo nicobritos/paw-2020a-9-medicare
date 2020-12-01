80958d3ed30c3fffc7009e5c130509e43708629d
nicbritos <nicbritos@hotmail.com>
2020-12-01 17:23:46 -0300

    [Vue] Removed MIME type from CountryService interface

--- vueapp/src/logic/interfaces/services/CountryService.ts
+++ vueapp/src/logic/interfaces/services/CountryService.ts
@@ -1,11 +1,6 @@
 import {Country} from '~/src/logic/models/Country';
 import {Nullable} from '~/src/logic/models/utils/Utils';
 
-export const CountryMIME = {
-    LIST: 'application/vnd.country.list.get.v1+json',
-    GET: 'application/vnd.country.get.v1+json',
-};
-
 export interface CountryService {
     list(): Promise<Country[]>;
 
