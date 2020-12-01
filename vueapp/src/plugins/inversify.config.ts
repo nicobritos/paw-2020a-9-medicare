0643a9c923c416c9874f34ce1b43657a2ad7016c
nicbritos <nicbritos@hotmail.com>
2020-12-01 17:23:59 -0300

    [Vue] Binded CountryService to inversify

--- vueapp/src/plugins/inversify.config.ts
+++ vueapp/src/plugins/inversify.config.ts
@@ -1,6 +1,15 @@
 import {Container} from 'inversify';
 import 'reflect-metadata';
+import {RestRepository} from '~/src/logic/interfaces/repositories/RestRepository';
+import TYPES from '~/src/logic/types';
+import {RestRepositoryImpl} from '~/src/logic/repositories/RestRepositoryImpl';
+import {CountryService} from '~/src/logic/interfaces/services/CountryService';
+import {CountryServiceImpl} from '~/src/logic/services/CountryServiceImpl';
 
 const container = new Container();
+
+container.bind<RestRepository>(TYPES.Repositories.RestRepository).to(RestRepositoryImpl).inSingletonScope();
+
+container.bind<CountryService>(TYPES.Services.CountryService).to(CountryServiceImpl).inSingletonScope();
 
 export default container;
