b2c13cc8ad1dff01e042a15f4d0f88fc2039a9b5
nicbritos <nicbritos@hotmail.com>
2020-12-18 19:27:38 -0300

    Fixed APIError not adding suberrors

--- webapp/src/main/java/ar/edu/itba/paw/webapp/models/error/APIError.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/models/error/APIError.java
@@ -1,13 +1,13 @@
 package ar.edu.itba.paw.webapp.models.error;
 
 import java.util.Collection;
-import java.util.Collections;
+import java.util.LinkedList;
 
 public class APIError extends APIBaseError {
     private final Collection<APISubError> subErrors;
 
     public APIError(int code, String message) {
-        this(code, message, Collections.emptyList());
+        this(code, message, new LinkedList<>());
     }
 
     public APIError(int code, String message, Collection<APISubError> subErrors) {
