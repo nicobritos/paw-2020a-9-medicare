2b4826a6db9ad3743d4dbcfc932f856b0a4ea536
nicbritos <nicbritos@hotmail.com>
2021-02-05 21:31:37 -0300

    Fixed localities 404

--- webapp/src/main/java/ar/edu/itba/paw/webapp/rest/LocalityResource.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/rest/LocalityResource.java
@@ -1,15 +1,10 @@
 package ar.edu.itba.paw.webapp.rest;
 
-import ar.edu.itba.paw.interfaces.services.CountryService;
 import ar.edu.itba.paw.interfaces.services.LocalityService;
-import ar.edu.itba.paw.interfaces.services.ProvinceService;
-import ar.edu.itba.paw.models.Country;
 import ar.edu.itba.paw.models.Locality;
-import ar.edu.itba.paw.models.Province;
 import ar.edu.itba.paw.webapp.media_types.ErrorMIME;
 import ar.edu.itba.paw.webapp.media_types.LocalityMIME;
 import ar.edu.itba.paw.webapp.media_types.MIMEHelper;
-import ar.edu.itba.paw.webapp.models.error.ErrorConstants;
 import ar.edu.itba.paw.webapp.rest.utils.GenericResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -24,47 +19,14 @@ import javax.ws.rs.core.Response;
 import java.util.Collection;
 import java.util.Optional;
 
-@Path("/")
+@Path("/localities")
 @Component
 public class LocalityResource extends GenericResource {
-    @Autowired
-    private CountryService countryService;
-    @Autowired
-    private ProvinceService provinceService;
     @Autowired
     private LocalityService localityService;
 
     @GET
-    @Path("/countries/{countryId}/provinces/{provinceId}/localities")
-    @Produces({LocalityMIME.GET_LIST, ErrorMIME.ERROR})
-    public Response getCollection(
-            @Context HttpHeaders httpheaders,
-            @PathParam("provinceId") Integer provinceId,
-            @PathParam("countryId") String countryId) {
-        MIMEHelper.assertServerType(httpheaders, LocalityMIME.GET_LIST);
-
-        Collection<Locality> localities;
-        Optional<Country> country = this.countryService.findById(countryId);
-        if (!country.isPresent()) {
-            throw this.unprocessableEntity(ErrorConstants.PROVINCE_GET_NONEXISTENT_COUNTRY);
-        }
-
-        Optional<Province> province = this.provinceService.findByCountryAndId(country.get(), provinceId);
-        if (!province.isPresent()) {
-            throw this.unprocessableEntity(ErrorConstants.LOCALITY_GET_NONEXISTENT_PROVINCE);
-        }
-
-        localities = this.localityService.findByProvince(province.get());
-
-        return Response
-                .ok()
-                .entity(localities)
-                .type(LocalityMIME.GET_LIST)
-                .build();
-    }
-
-    @GET
-    @Path("/localities")
+    @Path("")
     @Produces({LocalityMIME.GET_LIST, ErrorMIME.ERROR})
     public Response getAllCollection(@Context HttpHeaders httpheaders) {
         MIMEHelper.assertServerType(httpheaders, LocalityMIME.GET_LIST);
@@ -79,35 +41,7 @@ public class LocalityResource extends GenericResource {
     }
 
     @GET
-    @Path("/countries/{countryId}/provinces/{provinceId}/localities/{id}")
-    @Produces({LocalityMIME.GET, ErrorMIME.ERROR})
-    public Response getEntity(
-            @Context HttpHeaders httpheaders,
-            @PathParam("provinceId") Integer provinceId,
-            @PathParam("countryId") String countryId,
-            @PathParam("id") Integer id) {
-        MIMEHelper.assertServerType(httpheaders, LocalityMIME.GET);
-
-        if (id == null) throw this.missingPathParams();
-
-        Optional<Country> country = this.countryService.findById(countryId);
-        if (!country.isPresent()) {
-            throw this.unprocessableEntity(ErrorConstants.PROVINCE_GET_NONEXISTENT_COUNTRY);
-        }
-
-        Optional<Province> province = this.provinceService.findByCountryAndId(country.get(), provinceId);
-        if (!province.isPresent()) {
-            throw this.unprocessableEntity(ErrorConstants.LOCALITY_GET_NONEXISTENT_PROVINCE);
-        }
-
-        Optional<Locality> localityOptional = this.localityService.findByProvinceAndId(province.get(), id);
-        if (!localityOptional.isPresent()) throw this.notFound();
-
-        return Response.ok(localityOptional.get()).type(LocalityMIME.GET).build();
-    }
-
-    @GET
-    @Path("/localities/{id}")
+    @Path("{id}")
     @Produces({LocalityMIME.GET, ErrorMIME.ERROR})
     public Response getEntityById(
             @Context HttpHeaders httpheaders,
